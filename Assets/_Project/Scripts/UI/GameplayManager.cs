e984065651136ad813484f301a46a25973f36b17
Yunus Emre Usta <yunusemreusta.dev@gmail.com>
2026-03-04 03:23:34 +0300

    Faza 1 - Gameplay scene, DialogueEngine working end-to-end with branching

---
+++ Assets/_Project/Scripts/UI/GameplayManager.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using StoryGame.Core;
+using StoryGame.Characters;
+using StoryGame.Dialogue;
+using TMPro;
+
+namespace StoryGame.UI
+{
+    public class GameplayManager : MonoBehaviour
+    {
+        [Header("Diyalog UI")]
+        [SerializeField] private GameObject narrationPanel;
+        [SerializeField] private TextMeshProUGUI narrationText;
+
+        [SerializeField] private GameObject dialoguePanel;
+        [SerializeField] private TextMeshProUGUI speakerNameText;
+        [SerializeField] private TextMeshProUGUI dialogueText;
+
+        [SerializeField] private GameObject choicePanel;
+        [SerializeField] private Button[] choiceButtons;
+
+        [Header("HUD")]
+        [SerializeField] private TextMeshProUGUI diamondText;
+
+        [Header("Diyalog Verisi")]
+        [SerializeField] private DialogueData dialogueData;
+
+        private DialogueEngine _dialogueEngine;
+        private CharacterState _characterState;
+        private IDiamondService _diamondService;
+
+        private void Start()
+        {
+            _diamondService = ServiceLocator.Get<IDiamondService>();
+            UpdateDiamondUI();
+
+            // Seçili karakteri al
+            string characterId = PlayerPrefs.GetString("SelectedCharacter", "jenniffer");
+
+            // CharacterState oluştur
+            _characterState = new CharacterState();
+            _characterState.characterId = characterId;
+
+            // DialogueEngine kur
+            _dialogueEngine = gameObject.AddComponent<DialogueEngine>();
+            _dialogueEngine.OnNarrationNode += ShowNarration;
+            _dialogueEngine.OnDialogueNode += ShowDialogue;
+            _dialogueEngine.OnChoiceNode += ShowChoices;
+            _dialogueEngine.OnEpisodeEnded += OnEpisodeEnded;
+
+            // Panelleri kapat
+            HideAllPanels();
+
+            // Diyalogu başlat
+            if (dialogueData != null)
+                _dialogueEngine.StartEpisode(dialogueData, _characterState);
+            else
+                Debug.LogWarning("[GameplayManager] DialogueData atanmadı!");
+        }
+
+        private void Update()
+        {
+            // Ekrana tıklayınca ilerle
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (narrationPanel.activeSelf || dialoguePanel.activeSelf)
+                    _dialogueEngine.Advance();
+            }
+        }
+
+        private void ShowNarration(DialogueNode node)
+        {
+            HideAllPanels();
+            narrationPanel.SetActive(true);
+            narrationText.text = node.text;
+        }
+
+        private void ShowDialogue(DialogueNode node)
+        {
+            HideAllPanels();
+            dialoguePanel.SetActive(true);
+            speakerNameText.text = node.speaker;
+            dialogueText.text = node.text;
+        }
+
+        private void ShowChoices(DialogueNode node)
+        {
+            HideAllPanels();
+            choicePanel.SetActive(true);
+
+            for (int i = 0; i < choiceButtons.Length; i++)
+            {
+                if (i < node.choices.Count)
+                {
+                    choiceButtons[i].gameObject.SetActive(true);
+                    var choice = node.choices[i];
+                    var buttonText = choiceButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+                    if (buttonText != null)
+                        buttonText.text = choice.isDiamond ? $"💎 {choice.text} ({choice.diamondCost})" : choice.text;
+
+                    int index = i;
+                    choiceButtons[i].onClick.RemoveAllListeners();
+                    choiceButtons[i].onClick.AddListener(() => _dialogueEngine.SelectChoice(index));
+                }
+                else
+                {
+                    choiceButtons[i].gameObject.SetActive(false);
+                }
+            }
+        }
+
+        private void OnEpisodeEnded(EndingType ending)
+        {
+            Debug.Log($"[GameplayManager] Bölüm bitti! Ending: {ending}");
+            HideAllPanels();
+            // İleride ending sahnesine geçiş buraya eklenecek
+        }
+
+        private void HideAllPanels()
+        {
+            if (narrationPanel != null) narrationPanel.SetActive(false);
+            if (dialoguePanel != null) dialoguePanel.SetActive(false);
+            if (choicePanel != null) choicePanel.SetActive(false);
+        }
+
+        private void UpdateDiamondUI()
+        {
+            if (diamondText != null)
+                diamondText.text = _diamondService.GetAmount().ToString();
+        }
+
+        private void OnDestroy()
+        {
+            if (_dialogueEngine != null)
+            {
+                _dialogueEngine.OnNarrationNode -= ShowNarration;
+                _dialogueEngine.OnDialogueNode -= ShowDialogue;
+                _dialogueEngine.OnChoiceNode -= ShowChoices;
+                _dialogueEngine.OnEpisodeEnded -= OnEpisodeEnded;
+            }
+        }
+    }
+}
